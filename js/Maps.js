5f8cf142b9011a1cfefc6430536302bd20911f15
Joe Moses <jnmoses@gmail.com>
2013-11-09 10:52:27 -0600

    added map config documentation

--- js/Maps.js
+++ js/Maps.js
@@ -1,4 +1,26 @@
+/**
+ * Map configurations.
+ *
+ * A map configuration requires:
+ *  Textures
+ *    [x,y,width,height,image]
+ *    Just graphics with no collisions (background).
+ *  Blocks
+ *    [x,y,width,height,image]
+ *    Rectangles that cause collisions.
+ *
+ *
+ * Map dimensions are 970x546
+ *
+ * Available images:
+ *  dirt1: 32x32 of dirt
+ *  gret1: 32x32 grey metal type graphic
+ *
+ */
+
 var TTMaps = {
+
+  /** Classic map of 4 blocks. */
   Classic: {
     Textures: [
       [0,0,970,546,dirt1]
@@ -10,6 +32,8 @@ var TTMaps = {
       [678,360,96,96,grey1]
     ]
   },
+
+  /** 1 centered block. */
   Dirt1: {
     Textures: [
       [0,0,970,546,dirt1]
